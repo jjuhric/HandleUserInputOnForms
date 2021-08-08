9e88f892b3c979689025f710b2d2614d7b8b93a7
Jeff Uhrick <jeff.uhrick@kikoda.com>
2021-08-08 00:45:24 -0400

    whitespace

--- src/components/App.js
+++ src/components/App.js
@@ -15,13 +15,14 @@ class App extends React.Component {
             params: { query: term },
         });
         this.setState({ images: response.data.results });
+        
     }
 
     render() {
         return (
             <div className="ui container" style={ { marginTop: '10px' } }>
                 <SearchBar onSubmit={ this.onSearchSubmit } />
-                Found { this.state.images.length } images.
+                Found { this.state.images.length } images.                
             </div>
         )
     }
