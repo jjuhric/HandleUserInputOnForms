5366e9c414c78929c1bcfc5de174f2857e5e547d
Jeff Uhrick <jeff.uhrick@kikoda.com>
2021-08-08 00:21:33 -0400

    adding search with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+//    constructor(props) {
+//        super(props);
+//        this.state = { images: [] };
+//        this.onSearchSubmit = this.onSearchSubmit.bind(this);
+//    }
+    state = { images: [] };
 
-    onSearchSubmit( term ) {
-        console.log( term );
+    onSearchSubmit = async ( term ) => {
+        const response = await unsplash.get('/search/photos', {
+            params: { query: term },
+        });
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={ { marginTop: '10px' } }>
                 <SearchBar onSubmit={ this.onSearchSubmit } />
+                Found { this.state.images.length } images.
             </div>
         )
     }
