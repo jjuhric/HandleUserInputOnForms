efe7a9e1107fb18b4363babaa41d68e9b68539f8
Jeff Uhrick <jeff.uhrick@kikoda.com>
2021-08-08 21:39:22 -0400

    Adding image card and finalizing the styling for our image search app

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import ImageCard from './ImageCard'
+import './ImageList.css';
+
 
 const ImageList = (props) => {
     
-    const images = props.images.map(({description, id, urls}) => {
-        return <img alt={description} key={id} src={urls.regular} />
-    })
+    const images = props.images.map(image => <ImageCard key={image.id} image={image} />)
     
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 }
 
 export default ImageList;
